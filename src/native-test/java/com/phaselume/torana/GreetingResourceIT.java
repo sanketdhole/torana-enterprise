a7b2b8c568bc54cfa09721151adc4cfa92332cca
Sanket Dhole <sanket.dhole1997@gmail.com>
2026-09-20 21:33:30 +0530

    first commit

---
+++ src/native-test/java/com/phaselume/torana/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package com.phaselume.torana;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
